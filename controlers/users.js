aa78bef54bf371d8201571acddce3f23f7196b71
kaw393939 <keith@webizly.com>
2013-05-26 06:27:53 +0000

    added users controler

---
+++ controlers/users.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose')
+  , User = mongoose.model('User');
+  
+exports.list = function (req, res) {
+    User.find({}, function (err, users) {
+      res.render('users', { title: 'Express', users: users, user: req.user });
+    });
+  }
