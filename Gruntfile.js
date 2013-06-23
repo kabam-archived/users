23978f8f4c900c0919a6a3ec388b0e57f19109a7
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2013-06-23 21:31:58 +0700

    Reformat the tabs

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,54 +1,53 @@
 module.exports = function(grunt) {
+  grunt.initConfig({
+    jshint: {
+      all: [
+      'Gruntfile.js',
+      'lib/**/*.js',
+      'config/**/*.js',
+      'controllers/**/*.js',
+      'models/**/*.js',
+      'test/**/*.js'
+      ]
+    },
 
-	grunt.initConfig({
-		jshint: {
-			all: [
-        'Gruntfile.js',
-        'lib/**/*.js',
-        'config/**/*.js',
-        'controllers/**/*.js',
-        'models/**/*.js',
-        'test/**/*.js'
-			]
-		},
+    mochacli: {
+     options: {
+      require: ['should'],
+      reporter: 'spec',
+      bail: true,
+      recursive: true,
+      timeout: '15s'
+    },
+    all: ['test/*.js']
+  },
 
-		mochacli: {
-			options: {
-				require: ['should'],
-				reporter: 'spec',
-				bail: true,
-				recursive: true,
-				timeout: '15s'
-			},
-			all: ['test/*.js']
-		},
-
-    jsdoc: {
-      dist: {
-        src: [
-          'app.js',
-          'lib/**/*.js',
-          'config/**/*.js',
-          'controllers/**/*.js',
-          'models/**/*.js',
-          'test/**/*.js',
-          'README.md'
-        ],
-        options: {
-          destination: 'client/app/docs',
-          configure: 'config/conf.json'
-        }
+  jsdoc: {
+    dist: {
+      src: [
+      'app.js',
+      'lib/**/*.js',
+      'config/**/*.js',
+      'controllers/**/*.js',
+      'models/**/*.js',
+      'test/**/*.js',
+      'README.md'
+      ],
+      options: {
+        destination: 'client/app/docs',
+        configure: 'config/conf.json'
       }
     }
+  }
 
-	});
+});
 
 	// Load the plugin that provides the 'grunt-mocha-cli' task.
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-mocha-cli');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha-cli');
   grunt.loadNpmTasks('grunt-jsdoc');
 
-	grunt.registerTask('lint', ['jshint']);
-	grunt.registerTask('test', ['mochacli']);
+  grunt.registerTask('lint', ['jshint']);
+  grunt.registerTask('test', ['mochacli']);
   grunt.registerTask('default', ['mochacli', 'jshint', 'jsdoc']);
 };
