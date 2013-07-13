b268d67d5331bb965e217584516b0f2639b6ec8b
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2013-07-14 05:38:03 +0700

    Chore: Remove the login things and initial display Fix #10

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,47 +1,11 @@
 'use strict';
 
 angular.module('usersApp', [])
-  .config(function ($routeProvider) {
+  .config(function($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
-      })
-      .when('/signup', {
-        templateUrl: 'views/signup.html',
-        controller: 'SignupCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .when('/signout', {
-        templateUrl: 'views/signout.html',
-        controller: 'SignoutCtrl'
-      })
-      .when('/auth/twitter', {
-        templateUrl: 'views/auth/twitter.html',
-        controller: 'AuthTwitterCtrl'
-      })
-      .when('/auth/facebook', {
-        templateUrl: 'views/auth/facebook.html',
-        controller: 'AuthFacebookCtrl'
-      })
-      .when('/auth/github', {
-        templateUrl: 'views/auth/github.html',
-        controller: 'AuthGithubCtrl'
-      })
-      .when('/auth/linkedin', {
-        templateUrl: 'views/auth/linkedin.html',
-        controller: 'AuthLinkedinCtrl'
-      })
-      .when('/auth/meetup', {
-        templateUrl: 'views/auth/meetup.html',
-        controller: 'AuthMeetupCtrl'
-      })
-      .when('/login', {
-        templateUrl: 'views/login.html',
-        controller: 'LoginCtrl'
       })
       .otherwise({
         redirectTo: '/'
--- app/scripts/controllers/login.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('usersApp')
-  .controller('LoginCtrl', function($scope) {
-    $scope.heading = 'Login to My Web Class';
-    $scope.login = function() {
-      // do something here
-    };
-  });
